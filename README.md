d7ec0c6f538ca3c88f2581b5f23242286ce70254
Larry Meaney <larry_meaney@iname.com>
2020-04-01 17:12:42 -0700

    bind specific Vendor:Device
    This implements #2. I also reworded some error messages to more consistently use "Warning" vs "Error" (Errors exit, Warnings do not)
    
    Sorry, I did not apply the changes to the ruby script

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # vfio-pci-bind
 
-This script takes a Domain:Bus:Device.Function string of the form "0000:00:00.0" as command line argument and:
+This script takes two parameters:
 
-1. Unbinds all devices that are in the same iommu group as the supplied device from their current driver (except PCIe bridges).
-2. Binds to vfio-pci:
-    1. The supplied device.
-    2. All devices that are in the same iommu group.
-3. Transfers ownership of the respective iommu group inside /dev/vfio to $SUDO_USER
+- `Domain:Bus:Device.Function` (required) i.e. `0000:00:00.0`
+- `Vendor:Device` (optional) i.e. `0000:0000`
 
-__Script must be executed via sudo!__
+and then:
+
+1. Verifies that the optional `Vendor:Device` matches the Vendor:Device currently at the requested `Domain:Bus:Device.Function` PCI address. If they do not match, exit without binding the requested PCI address. The goal is to prevent the wrong device from being bound to vfio-pci after a hardware change.
+2. Unbinds all devices that are in the same iommu group as the supplied device from their current driver (except PCIe bridges).
+3. Binds to vfio-pci:
+   1. The supplied device.
+   2. All devices that are in the same iommu group.
+4. Transfers ownership of the respective iommu group inside /dev/vfio to \$SUDO_USER
+
+**Script must be executed via sudo!**
 
 ## License
 
--- vfio-pci-bind.sh
+++ vfio-pci-bind.sh
@@ -32,26 +32,35 @@
 #
 # =============================================================================
 #
-# This script takes a Domain:Bus:Device.Function string of the form
-# "0000:00:00.0" as command line argument and:
+# This script takes two parameters:
+#   <Domain:Bus:Device.Function> (required) i.e. 0000:00:00.0
+#   <Vendor:Device> (optional) i.e. 0000:0000
+# and then:
 #
-#  (1) Unbinds all devices that are in the same iommu group as the supplied
+#  (1) Verifies that the optional <Vendor:Device> matches the Vendor:Device 
+#      currently at the requested <Domain:Bus:Device.Function> PCI address. 
+#      If they do not match, exit without binding the requested PCI address. 
+#      The goal is to prevent the wrong device from being bound to vfio-pci 
+#      after a hardware change.
+#
+#  (2) Unbinds all devices that are in the same iommu group as the supplied
 #      device from their current driver (except PCIe bridges).
 #
-#  (2) Binds to vfio-pci:
-#    (2.1) The supplied device.
-#    (2.2) All devices that are in the same iommu group.
+#  (3) Binds to vfio-pci:
+#    (3.1) The supplied device.
+#    (3.2) All devices that are in the same iommu group.
 #
-#  (3) Transfers ownership of the respective iommu group inside /dev/vfio
+#  (4) Transfers ownership of the respective iommu group inside /dev/vfio
 #      to $SUDO_USER
 #
 # Script must be executed via sudo
 
 BDF_REGEX="^[[:xdigit:]]{2}:[[:xdigit:]]{2}.[[:xdigit:]]$"
 DBDF_REGEX="^[[:xdigit:]]{4}:[[:xdigit:]]{2}:[[:xdigit:]]{2}.[[:xdigit:]]$"
+VD_REGEX="^[[:xdigit:]]{4}:[[:xdigit:]]{4}$"
 
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 1>&2
+    echo "Error: This script must be run as root" 1>&2
     exit 1
 fi
 
@@ -61,23 +70,34 @@ elif [[ $1 =~ $BDF_REGEX ]]; then
     BDF="0000:$1"
     echo "Warning: You did not supply a PCI domain, assuming $BDF" 1>&2
 else
-    echo "Please supply Domain:Bus:Device.Function of PCI device in form: dddd:bb:dd.f" 1>&2
+    echo "Error: Please supply Domain:Bus:Device.Function of PCI device in form: dddd:bb:dd.f" 1>&2
     exit 1
 fi
 
 TARGET_DEV_SYSFS_PATH="/sys/bus/pci/devices/$BDF"
 
 if [[ ! -d $TARGET_DEV_SYSFS_PATH ]]; then
-    echo "There is no such device"
+    echo "Error: Device ${BDF} does not exist, unable to bind device" 1>&2
     exit 1
 fi
 
 if [[ ! -d "$TARGET_DEV_SYSFS_PATH/iommu/" ]]; then
-    echo "No signs of an IOMMU. Check your hardware and/or linux cmdline parameters." 1>&2
-    echo "Use intel_iommu=on or iommu=pt iommu=1" 1>&2
+    echo "Error: No signs of an IOMMU. Check your hardware and/or linux cmdline parameters. Use intel_iommu=on or iommu=pt iommu=1" 1>&2
     exit 1
 fi
 
+if [[ $2 =~ $VD_REGEX ]]; then
+    if [[ $(lspci -n -s ${BDF} -d $2 2>/dev/null | wc -l) -eq 0 ]]; then
+        echo "Error: Vendor:Device $2 not found at ${BDF}, unable to bind device" 1>&2
+        exit 1
+    else
+        echo "Vendor:Device $2 found at ${BDF}"
+    fi
+else
+    echo "Warning: You did not specify a Vendor:Device in form vvvv:dddd, unable to validate ${BDF}" 1>&2
+fi
+
+unset dev_sysfs_paths
 for dsp in $TARGET_DEV_SYSFS_PATH/iommu_group/devices/*
 do
     dbdf=${dsp##*/}
@@ -91,7 +111,7 @@ for dsp in ${dev_sysfs_paths[@]}; do echo $dsp; done
 
 modprobe -i vfio-pci
 if [[ $? -ne 0 ]]; then
-    echo "Error probing vfio-pci"
+    echo "Error: Error probing vfio-pci" 1>&2
     exit 1
 fi
 
@@ -126,11 +146,12 @@ iommu_group=$(readlink $TARGET_DEV_SYSFS_PATH/iommu_group)
 iommu_group=${iommu_group##*/}
 chown $SUDO_UID:$SUDO_GID "/dev/vfio/$iommu_group"
 if [[ $? -ne 0 ]]; then
-    echo "Error adjusting group ownership"
+    echo "Error: unable to adjust group ownership of /dev/vfio/${iommu_group}" 1>&2
     exit 1
 fi
 
 printf "success...\n\n"
+echo "Device $2 at ${BDF} bound to vfio-pci"
 echo 'Devices listed in /sys/bus/pci/drivers/vfio-pci:'
 ls -l /sys/bus/pci/drivers/vfio-pci | egrep [[:xdigit:]]{4}:
 printf "\nls -l /dev/vfio/\n"

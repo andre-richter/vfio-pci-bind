4fb8cab07ac88cbf9d1c7ddb8bef4a4d8513148b
Andre Richter <andre-richter@users.noreply.github.com>
2021-04-29 07:54:32 +0200

    Update vfio-pci-bind.sh

--- vfio-pci-bind.sh
+++ vfio-pci-bind.sh
@@ -5,7 +5,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2015 Andre Richter
+# Copyright (c) 2015-2021 Andre Richter
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
